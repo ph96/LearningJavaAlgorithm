99f2c458416cf3a4f24c9288dd4dc816eeb5dfd4
ph96 <1436302114@qq.com>
2019-09-29 00:26:41 +0800

    完善注释并修改README.md

--- src/two_sum/TwoSum.java
+++ src/two_sum/TwoSum.java
@@ -5,19 +5,22 @@ package two_sum;
  */
 public class TwoSum {
     /**
-     * @param nums
-     * @param target
+     *  返回随机整数数组nums中两数之和为目标值target的索引值</br>
+     *  如果未找到任意两数之和为目标值target则返回null
+     * @param nums  随机整数数组
+     * @param target  目标值
      * @return
      */
     public int[] twoSum(int[] nums, int target) {
         for(int i = 0;i<nums.length;i++){
             for(int j = i+1;j < nums.length; j++){
                 if(nums[i] + nums[j] == target){
-                    System.out.println("["+i+","+j+"]");
+                    //System.out.println("["+i+","+j+"]");
                     return new int[]{i,j};
                 }
             }
         }
-        return new int[3];
+        return null;
     }
+
 }
